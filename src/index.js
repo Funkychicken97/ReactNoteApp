37cd61d513424f0ccc4736b1245bce54d4d696ed
Samuel Lovitt <samuellovitt@Samuels-MBP.hsd1.ms.comcast.net>
2019-12-12 16:02:23 -0600

    vulnerabilities updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Board from './components/Board.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	 apiKey: "AIzaSyBJpQJLAoVDYkjvYp3n-CAQA0gVAFbDw-Q",
+    authDomain: "the-app-e3e2a.firebaseapp.com",
+    databaseURL: "https://the-app-e3e2a.firebaseio.com",
+    projectId: "the-app-e3e2a",
+    storageBucket: "the-app-e3e2a.appspot.com",
+    messagingSenderId: "1086434860667",
+    appId: "1:1086434860667:web:ca8d317f4c71fe58"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+ReactDOM.render(<Board dbRef={firebase.database().ref()} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
